8c358912c028e647f184f665d12ce037f90338f7
apm <amoosavi@kth.se>
2017-02-07 17:34:20 +0100

    comments to PersonFacade, removed AbstractFacade

--- recruitmentapp/src/main/java/se/kth/ict/iv1201/recruitmentapp/controller/PersonFacade.java
+++ recruitmentapp/src/main/java/se/kth/ict/iv1201/recruitmentapp/controller/PersonFacade.java
@@ -9,21 +9,34 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import se.kth.ict.iv1201.recruitmentapp.model.Person;
 
+/**
+ * A controller. Calls to the model that are executed because of an action taken
+ * by the user pass through here.
+ */
 @Stateless
-public class PersonFacade extends AbstractFacade<Person> {
+public class PersonFacade {
 
     @PersistenceContext(unitName = "se.kth.ict.iv1201_recruitmentapp_war_1.0-SNAPSHOTPU")
     private EntityManager em;
 
-    @Override
     protected EntityManager getEntityManager() {
         return em;
     }
 
-    public PersonFacade() {
-        super(Person.class);
-    }
-
+    /**
+     * Creates a new user (Person) with the specified parameters and persists
+     * its data.
+     *
+     * @param username user's username.
+     * @param password user's password.
+     * @param name user's name.
+     * @param surname user's surname.
+     * @param ssn user's Social Security Number (ssn).
+     * @param email user's email.
+     * @param role user's role.
+     *
+     * @throws EJBException
+     */
     public void Save(String username, String password, String name, String surname, String ssn, String email, long role) {
         try {
             Person mPerson = new Person(username, password, name, surname, ssn, email, role);
